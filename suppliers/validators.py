8f56fa7f77c1070b82831bbbec7da8ff8eaeb5d7
Gonzalo Slaboch <gslaboch@gmail.com>
2016-02-24 14:55:36 -0300

    Phone validation

--- suppliers/validators.py
+++ suppliers/validators.py
@@ -2,5 +2,5 @@ from django.core.validators import EmailValidator, RegexValidator
 
 email_validator = EmailValidator()
 
-phone_validator = RegexValidator(regex=r'^\+?[\d\s]{6,15}$', 
-                message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+phone_validator = RegexValidator(regex=r'^\+?\d{0,5}\s?\d{5,15}$', 
+                message="Phone number invalid")
